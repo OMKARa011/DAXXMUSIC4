c46b3fc076a2e582573f8f056760ebd8946b6769
ㅤㅤㅤ⏤͟͟͞͞•𓆩𖤍𓆪ꪾ 𝐃𝜩‌𝐕𝐈𝐋⛧‌ٖٖٖٖٖٖٜٖٖٖٖٖٖٜٖㅤ [•ᴧғᴋ•] ㅤㅤ <105618799+OMKARa011@users.noreply.github.com>
2024-06-21 13:53:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -69,8 +69,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ALLTYPECC")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/gitwizardbypass")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Violet_Update")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Voilet_Chat")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
@@ -143,13 +143,13 @@ confirmer = {}
 # ------------------------------------------------------------------------
 # ------------------------------------------------------------------------
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/cfbdee8103102bcb2e5da.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/ad94e8a360edeac9bc3a7.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://telegra.ph/file/00360393a15daf7fc4e9d.jpg"
 )
 PLAYLIST_IMG_URL = "https://telegra.ph/file/d723f4c80da157fca1678.jpg"
-STATS_IMG_URL = "https://telegra.ph/file/d30d11c4365c025c25e3e.jpg"
+STATS_IMG_URL = "https://telegra.ph/file/ad94e8a360edeac9bc3a7.jpg"
 TELEGRAM_AUDIO_URL = "https://telegra.ph/file/48f39202823b358203234.jpg"
 TELEGRAM_VIDEO_URL = "https://telegra.ph/file/e575ae40d6635250974e1.jpg"
 STREAM_IMG_URL = "https://telegra.ph/file/03efec694e41e891b29dc.jpg"
